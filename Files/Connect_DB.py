48ef6cf59f4fd07506632b1255a1252e25da5922
Joao Gabriel <jgafarias1@gmail.com>
2025-02-12 18:48:35 -0300

    Update

--- Files/Connect_DB.py
+++ Files/Connect_DB.py
@@ -1,4 +1,4 @@
 import sqlite3  # Importa o módulo sqlite3 para trabalhar com bancos de dados SQLite
 
 def connect():
-    return sqlite3.connect('Test.db')  # Retorna a conexão com o banco de dados
+    return sqlite3.connect('DB-Python/Test.db')  # Retorna a conexão com o banco de dados
